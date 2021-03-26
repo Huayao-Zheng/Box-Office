d6a5129977dd49e548452a161bd045e1616298ee
lonelybuddy <henryzheng92@gmail.com>
2021-03-26 13:33:45 -0400

    defined them for styled components and created a few styled components

--- src/components/MainpageLayout.js
+++ src/components/MainpageLayout.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navs from './Navs';
-import TItle from './TItle';
+import Title from './Title';
 
 const MainpageLayout = ({ children }) => (
   <div>
-    <TItle title="Box Office" subtitle="Looking for a movie or actor" />
+    <Title title="Box Office" subtitle="Looking for a movie or actor" />
     <Navs />
 
     {children}
